2900e81d0520e9ac92bb8d356f480c26dc7dd7e7
Carlos H. Mendoza-Cardenas <cmendoza@udel.edu>
2021-02-21 15:52:05 -0500

    Refactor to simplify non-overlapping case
    Make num. of train and test windows an optional argument, and add two
    additional optional arguments to allow the request of windows using
    percentages. This refactoring simplifies the non-overlapping case, but
    it probably overcomplicated the overlapping case.

--- splitdata/splitdata.m
+++ splitdata/splitdata.m
@@ -1,6 +1,5 @@
-function [fnames, i_start] = splitdata(dirpath, winlen, n_train, n_test, varargin)
+function [fnames, i_start] = splitdata(dirpath, winlen, varargin)
 % Pick multichannel windows of length winlen at random for training and testing
-% of CSP method
 %
 % [fnames, i_start] = splitdata(dirpath, winlen, n_train, n_test, varargin)
 %
@@ -10,19 +9,23 @@ function [fnames, i_start] = splitdata(dirpath, winlen, n_train, n_test, varargi
 %   Absolute path to the data folder
 % winlen (int):
 %   Length of the instance (window)
+%
+% Optional parameters
+% -------------------
 % n_train (int):
 %   Number of training instances
 % n_test (int):
 %   Number of test instances
-% 
-% Optional parameters
-% -------------------
+% prctage (float):
+%   Percentage of the data in the folder to be used. A number in [0 1].
+% train_prctage (float):
+%   Percentage of train data.  A number in [0 1].
 % overlap (logical):
 %   true if overlapping windows for train/test set are allowed, false
 %   otherwise. Default: false.
 % n_subdiv (int):
 %   Number of non-overlapping subintervals where to pick from the
-%   overlapping train/test windows. The length of the subintervals on each 
+%   overlapping train/test windows. The length of the subintervals on each
 %   epoch is equal. Subintervals are assigned at random to train/test sets.
 %   It must be an even number. Default: 2.
 % start_gap (int):
@@ -31,11 +34,11 @@ function [fnames, i_start] = splitdata(dirpath, winlen, n_train, n_test, varargi
 %
 % Returns
 % -------
-% fnames (str): cell array with the filenames of the epochs from where 
+% fnames (str): cell array with the filenames of the epochs from where
 %               windows were picked
 %
 % i_start (uint32): cell array with start index of the windows within each epoch
-%      
+%
 % i_start{i,k}(j) is the start index of the j-th window in the file
 % fnames{i}, with k=1 for train set and k=2 for test set
 %
@@ -44,33 +47,42 @@ function [fnames, i_start] = splitdata(dirpath, winlen, n_train, n_test, varargi
 % variable 'epoch', an CxT matrix, with C the number of channels, and T
 % the number of time points. T might vary among epochs (files).
 %
-% If overlap is false, the number of random starts (e.g., Monte Carlo 
+% If overlap is false, the number of random starts (e.g., Monte Carlo
 % simulations) is limited to winlen-1. There are much more random starts if
 % overlap is true.
 
 %% Defaults for optional parameters
+n_train = []; n_test = []; prctage = 100; train_prctage = 80;
 overlap = false; n_subdiv = 2; start_gap = winlen;
 
 %% Parse arguments
-if nargin < 4
+if nargin < 2
     error('Wrong number of arguments');
-elseif nargin > 4
+elseif nargin > 2
     n_varargin = length(varargin);
     assert(mod(n_varargin,2) == 0, 'Wrong number of optional arguments');
     for i_opt = 1:2:n_varargin
         switch varargin{i_opt}
+            case 'n_train'
+                n_train = varargin{i_opt+1};
+            case 'n_test'
+                n_test = varargin{i_opt+1};
+            case 'prctage'
+                prctage = varargin{i_opt+1};
+            case 'train_prctage'
+                train_prctage = varargin{i_opt+1};
             case 'overlap'
                 overlap = varargin{i_opt+1};
             case 'n_subdiv'
                 n_subdiv = varargin{i_opt+1};
             case 'start_gap'
-                start_gap = varargin{i_opt+1};            
+                start_gap = varargin{i_opt+1};
             otherwise
                 error('Wrong optional arguments');
         end
     end
 end
-         
+
 %% Get file list
 expr = '^rx(?<epoch_id>\d+).mat$';
 S = dir(dirpath);
@@ -79,35 +91,19 @@ epoch_files = ~cellfun(@isempty, regexp(fnames, expr));
 fnames = fnames(epoch_files);
 n_epochs = length(fnames);
 
-%% Pick number of epochs (files)
-% If the number of epochs is too big, pick only max(n_train, n_test). In
-% that way, at least one window is picked per epoch
-max_epochs = max(n_test, n_train);
-if n_epochs > max_epochs
-    % Randomly shuffle the file names
-    fnames = fnames(randperm(n_epochs, max_epochs));
-    n_epochs = max_epochs;
-end
-
 %% Get the length of each epoch to estimate how many windows should be picked
 pnts = zeros(n_epochs,1);
 for i_epoch = 1:n_epochs
-    %% Load the data    
+    %% Load the data
     fpath = fullfile(dirpath, fnames{i_epoch});
     mObj = matfile(fpath);
-    pnts(i_epoch) = size(mObj, 'epoch', 2);    
+    pnts(i_epoch) = size(mObj, 'epoch', 2);
 end
 [pnts, isort] = sort(pnts, 'descend');
 fnames = fnames(isort);
 
-% Discard epochs that can not accomodate at least 1 window/subdiv or 1
-% window
-if overlap
-    % FIXME: This might be wrong
-    ind = pnts < (winlen+start_gap)*n_subdiv;
-else
-    ind = pnts < winlen+start_gap;
-end
+% Discard epochs that can not accomodate at least one window
+ind = pnts < winlen+start_gap;
 
 if any(ind)
     fprintf('Discarding %d out %d epochs that are not long enoug\n', ...
@@ -117,163 +113,194 @@ if any(ind)
     n_epochs = length(pnts);
 end
 
-%% Make sure that there is enough data available
-L = sum(pnts);  % Total number of samples available (per channel)
-if overlap
-    n_win = n_train + n_test;
-    % Start indexes can go in [1 l-start_gap], where l is the length of a
-    % subdivision of an epoch. So, start_gap samples are reserved per
-    % subdivision per epoch.
-    % FIXME: how big can be winlen?
-    max_win = L - n_subdiv * start_gap * n_epochs;
-    data_prctage = n_win / max_win;
-else
-    % add 1 start_gap/epoch for a random start. NOTE: This limits the number of
-    % random starts (Monte Carlos simulations) to start_gap - 1:
-    n_win = n_train + n_test;
-    data_prctage = winlen * n_win / (L-start_gap*n_epochs);  % percentage of data requested
-end
-
-assert(data_prctage<=1, 'Not enough data in %d epochs!', n_epochs);
-
-if overlap
-    tot_n_win = floor((pnts-n_subdiv*start_gap)*data_prctage);
-else
-    tot_n_win = floor((pnts-start_gap)*data_prctage/winlen);
-end
-
-max_win = sum(tot_n_win);
-if max_win < n_win
-    fprintf('Max number of windows at %.3f%% percentage: %d\n',...
-        data_prctage*100, max_win);
-    error('Not enough data!')
-end
-
-train_prctage = n_train / (n_train + n_test);
-
-n_win = zeros(n_epochs, 2);
-n_win(:,1) = floor(train_prctage * tot_n_win);  % Num. of train windows
-n_win(:,2) = tot_n_win - n_win(:,1);  % Num of test windows
-
-I = any(n_win<0, 2);
-n_win(I,:) = [];
-pnts(I) = [];
-fnames(I) = [];
-
-%% Adjust number of windows per epoch to match requested number
-ndiff = sum(n_win) - [n_train n_test];
-misswin = abs(diff(abs(ndiff)));
-% we are missing windows: add one window from longest epochs
-if misswin
-    n_win(1:misswin, ndiff<0) = n_win(1:misswin, ndiff<0) + 1;
-    ndiff = sum(n_win) - [n_train n_test];
-end
-
-if any(ndiff < 0)
-    % Adjust number of train/test windows if not enough windows on one set
-    k = abs(min(ndiff));
-    [~, isort] = sort(sum(n_win, 2), 'descend');
-    top_k_epochs = isort(1:k); % train
-    n_win(top_k_epochs, ndiff<0) = n_win(top_k_epochs, ndiff<0) + 1;
-    n_win(top_k_epochs, ndiff>0) = n_win(top_k_epochs, ndiff>0) - 1;
-    ndiff = sum(n_win) - [n_train n_test];
-end
-
-if any(ndiff > 0)
-    % If there is a surplus of k windows, substract one window from the top
-    % k epochs with most number of windows
-    [~, isort] = sort(n_win, 'descend');
-    top_k_epochs = isort(1:ndiff(1),1); % train
-    n_win(top_k_epochs,1) = n_win(top_k_epochs,1) - 1; 
-    top_k_epochs = isort(1:ndiff(2),2); % test
-    n_win(top_k_epochs,2) = n_win(top_k_epochs,2) - 1;     
-end
-
-I = all(n_win == [0 0], 2);
-n_win(I,:) = [];
-n_epochs = size(n_win, 1);
-pnts(I) = [];
-fnames(I) = [];
-ndiff = sum(n_win) - [n_train n_test];
-assert(all(ndiff==0), 'Something went wrong!')
-
-%% Generate start indices for train and test windows on each epoch
-if n_test == 0
-    i_start = cell(n_epochs, 1);
-else
-    i_start = cell(n_epochs, 2);
-end
-
-for i_epoch = 1:n_epochs
-    ni_train = n_win(i_epoch,1); % Num. of train windows in i-th epoch
-    ni_test = n_win(i_epoch,2); ni = [ni_train ni_test];
+if not(overlap)    
+    %% ==== non-overlaping windows ============
     
-    rand_start =  randperm(start_gap, 1);
+    avail_pnts = pnts - start_gap;
+    max_win = floor(avail_pnts/winlen);
+    tot_max_win = sum(max_win);
+    fprintf('Max. number of non-overlapping windows in %d epochs: %d\n',...
+        n_epochs, tot_max_win);
     
-    if overlap
-        if n_test == 0
-            i_start{i_epoch, 1} = uint32(rand_start + ...
-                randperm(pnts(i_epoch)-start_gap, ni_train));
-        else
-            sublen = floor(pnts(i_epoch)/n_subdiv);
-            offset = 0:sublen:sublen*(n_subdiv-1); % start indices of subintervals
-            offset = offset(randperm(n_subdiv)); % shuffle
-            offset = reshape(offset, n_subdiv/2, 2); % assign to train/test
-            
-            % Number of train/test windows on each subinterval
-            sub_ni = floor(ni / (n_subdiv/2));
-            sub_ni = repmat(sub_ni, n_subdiv/2, 1);
-            kadd = ni - sum(sub_ni);
-            sub_ni(1:kadd(1),1) = sub_ni(1:kadd(1),1) + 1;
-            sub_ni(1:kadd(2),2) = sub_ni(1:kadd(2),2) + 1;
-            
-            i_start{i_epoch,1} = zeros(1, ni_train, 'uint32');
-            i_start{i_epoch,2} = zeros(1, ni_test, 'uint32');
-            win_cnt = [1 1];
-            for i_sub = 1:n_subdiv/2
-                
-                % train
-                ii_start = rand_start + randperm(sublen-start_gap, sub_ni(i_sub,1));
-                ii_start = ii_start + offset(i_sub,1);
-                i_start{i_epoch,1}(win_cnt(1):win_cnt(1)+sub_ni(i_sub,1)-1) = ...
-                    uint32(ii_start);
-                win_cnt(1) = win_cnt(1) + sub_ni(i_sub,1);
-                
-                % test
-                ii_start = rand_start + randperm(sublen-start_gap, sub_ni(i_sub,2));
-                ii_start = ii_start + offset(i_sub,2);
-                i_start{i_epoch,2}(win_cnt(2):win_cnt(2)+sub_ni(i_sub,2)-1) = ...
-                    uint32(ii_start);
-                win_cnt(2) = win_cnt(2) + sub_ni(i_sub,2);
-            end
-        end
+    if ~isempty(n_train) && ~isempty(n_test)
+        train_prctage = n_train / (n_train + n_test);
+        test_prctage = 1 - train_prctage;
+        tot_req_win = n_train + n_test;
+        assert(tot_req_win <= tot_max_win, 'Not enough data!');
     else
-        Lt = pnts(i_epoch) - start_gap; % Num. of points available for sampling
-        max_step = floor(Lt/sum(ni));
-        if max_step < winlen
-            max_step = winlen;
-        end        
-        
-        % Generate (ordered) start indexes that are at least winlen apart
-        % (non-ovelapping windows)
-        ii_start = rand_start:max_step:rand_start+max_step*(sum(ni)-1);                     
-        ichoose = randperm(sum(ni)); % randomly shuffle ordered start indexes
-        i_start{i_epoch,1} = uint32(ii_start(ichoose(1:ni(1))));  % train
-        if n_test > 0
-            i_start{i_epoch,2} = uint32(ii_start(ichoose(ni(1)+1:end))); % test
-        end
+        test_prctage = 1 - train_prctage;
+        n_train = floor(train_prctage * prctage * tot_max_win);
+        n_test = ceil(test_prctage * prctage * tot_max_win);
     end
+    
+    %% Generate max. num of window indices at random per epoch
+    rand_start = randi(start_gap, n_epochs, 1);
+    win_ind = arrayfun(@(x,y) x + 1:winlen:y-winlen+1, ...
+        rand_start, avail_pnts, 'UniformOutput', false);
+    win_pep = cellfun(@length, win_ind);
+    
+    %% Compute number of train windows to draw from each epoch
+    win_train = floor(win_pep * train_prctage);
+    rem_win = n_train - win_train;
+    % add 1 window to top epochs to achieve desired number of test windows
+    win_train(1:rem_win) = win_train(1:rem_win) + 1;
+    
+    %% Compute number of test windows to draw from each epoch
+    win_test = floor(win_pep * test_prctage);
+    rem_win = n_test - win_test;
+    % add 1 window to top epochs to achieve desired number of test windows
+    win_test(1:rem_win) = win_test(1:rem_win) + 1;
+    
+    i_start = cell(n_epochs, 2);
+    
+    for i_epoch = 1:n_epochs
+        %% Pick windows at random
+        
+        % Pick indices for training windows
+        idx = randperm(win_pep(i_epoch), win_train(i_epoch));
+        i_start{i_epoch, 1} = win_ind{i_epoch}(idx);
+        
+        % Discard train indices to ensure that test indices are different
+        win_pep(i_epoch) = win_pep(i_epoch) - win_train(i_epoch);
+        win_ind{i_epoch}(idx) = [];
+        
+        % Pick indices for test windows
+        idx = randperm(win_pep(i_epoch), win_test(i_epoch));
+        i_start{i_epoch, 2} = win_ind{i_epoch}(idx);
+        
+    end
+    
+    % i_start{:,2} is empty if n_test == 0
+    i_start = i_start(~cellfun(@isempty, i_start));
     
     % Make sure that we are not going out of boundaries
     max_i_start = max(horzcat(i_start{i_epoch,:}));
     assert(max_i_start+winlen-1 <= pnts(i_epoch), 'Something went wrong!');
+else
+    %% ======== Overlapping windows  ================
     
-    if n_test > 0
-        % Make sure that there is no overlapping between train and test set
-        rep_ni_test = repmat(i_start{i_epoch,2}', 1, ni_train);
-        dist = abs(double(rep_ni_test)-double(i_start{i_epoch,1}));
-        assert(all(dist(:) >= winlen), 'Train and test windows are overlapping!');
+    %% Computing max. number of windows
+    % TODO: This is likely wrong, as it is not considering the case where
+    % n_train > n_test: the subdivision for training should be longer.
+    max_win = zeros(n_epochs, 1);
+    %% epochs that can acommodate at least one window per sub-division.
+    ind1 = pnts >= n_subdiv * (winlen+start_gap);
+    divlen = pnts(ind1) - n_subdiv * start_gap;
+    max_win(ind1) = divlen - winlen;
+    
+    %% epochs that can acommodate at least one window for two sub-divisions.
+    ind2 = pnts >= 2 * (winlen+start_gap) & pnts < n_subdiv * (winlen+start_gap);
+    divlen = pnts(ind2) - 2 * start_gap;
+    max_win(ind2) = divlen - winlen;
+    
+    %% shorter epochs
+    ind3 = pnts < 2 * (winlen+start_gap);
+    divlen = pnts(ind3) - start_gap;
+    max_win(ind3) = divlen - winlen;
+    
+    tot_max_win = sum(max_win);
+    fprintf('Max. number of overlapping windows in %d epochs: %d\n',...
+        n_epochs, tot_max_win);
+    
+    if ~isempty(n_train) && ~isempty(n_test)
+        train_prctage = n_train / (n_train + n_test);
+        test_prctage = 1 - train_prctage;
+        tot_req_win = n_train + n_test;
+        assert(tot_req_win <= tot_max_win, 'Not enough data!');
+    else
+        test_prctage = 1 - train_prctage;
+        n_train = floor(train_prctage * prctage * tot_max_win);
+        n_test = ceil(test_prctage * prctage * tot_max_win);
     end
-        
+    
+    if n_test == 0
+        avail_pnts = pnts - start_gap;
+        rand_start = randi(start_gap, n_epochs, 1);
+        win_ind = arrayfun(@(x,y) x + randperm(y-winlen+1), ...
+            rand_start, avail_pnts, 'UniformOutput', false);
+        win_pep = cellfun(@length, win_ind);
+        cumwin = cumsum(win_pep);
+        idx = cumwin <= n_train;
+        idx = find(idx, 1, 'last');
+        tot_win = cumwin(idx);
+        if tot_win == n_train
+            fnames = fnames(1:idx);
+            i_start = win_ind(1:idx);
+        else % n_train > tot_win
+            rem_win = n_train - tot_win;
+            fnames = fnames(1:idx+1);
+            i_start = [win_ind(1:idx); {win_ind{idx+1}(1:rem_win)}];
+        end
+        return;
+    end
+    
+    win_ind = cell(n_epochs, 2);
+    for i_epoch = 1:n_epochs
+        if any(i_epoch == find(ind1)) || any(i_epoch == find(ind2))
+            if any(i_epoch == find(ind2))
+                tn_subdiv = 1;
+            else
+                tn_subdiv = n_subdiv/2;
+            end
+            % epochs that can acommodate at least one window per sub-division.
+            train_divlen = floor(train_prctage*pnts(i_epoch)/tn_subdiv);
+            test_divlen = floor(test_prctage*pnts(i_epoch)/tn_subdiv);
+            pair_divlen = train_divlen + test_divlen;
+            pair_offset = 0:pair_divlen:pair_divlen*(tn_subdiv-1); % start indices of pair of subintervals
+            sub_offsets = [0 train_divlen];
+            shuffle_ind = randi(2, tn_subdiv); % Bernoulli with p=0.5, and values 1, and 2.
+            train_offset = pair_offset + sub_offsets(shuffle_ind);
+            test_offset = pair_offset + sub_offsets(3-shuffle_ind);
+            win_div_ind = cell(tn_subdiv, 2);
+            for i_div = 1:tn_subdiv-1
+                rand_start = randperm(start_gap, 1);
+                ii_start = rand_start + train_offset(i_div);
+                win_div_ind{i_div, 1} = ii_start + randperm(train_divlen-start_gap-winlen+1);
+                rand_start = randperm(start_gap, 1);
+                ii_start = rand_start + test_offset(i_div);
+                win_div_ind{i_div, 2} = ii_start + randperm(test_divlen-start_gap-winlen+1);
+            end
+            pair_divlen = pnts(i_epoch) - pair_divlen*(tn_subdiv-1);
+            train_divlen = train_prctage * pair_divlen;
+            test_divlen = pair_divlen - train_divlen;
+            rand_start = randperm(start_gap, 1);
+            ii_start = rand_start + train_offset(tn_subdiv);
+            win_div_ind{tn_subdiv, 1} = ii_start + randperm(train_divlen-start_gap-winlen+1);
+            rand_start = randperm(start_gap, 1);
+            ii_start = rand_start + test_offset(tn_subdiv);
+            win_div_ind{tn_subdiv, 2} = ii_start + randperm(test_divlen-start_gap-winlen+1);
+            win_ind{i_epoch, 1} = horzcat(win_div_ind{:,1});
+            win_ind{i_epoch, 2} = horzcat(win_div_ind{:,2});
+        else
+            % shorter epochs
+            rand_start = randperm(start_gap, 1);
+            win_ind{i_epoch, 1} = rand_start + randperm(pnts(i_epoch)-start_gap-winlen+1);
+        end
+    end
+    nn = [n_train n_test];
+    i_start = cell(n_epochs, 2);
+    for i_set = 1:2 % train/test
+        win_pep = cellfun(@length, win_ind(:,i_set));
+        cumwin = cumsum(win_pep);
+        idx = cumwin <= nn(i_set);
+        idx = find(idx, 1, 'last');
+        tot_win = cumwin(idx);
+        if tot_win == nn(1)
+            fnames = fnames(1:idx);
+            i_start(1:idx, i_set) = win_ind(1:idx, i_set);
+        else % n_train > tot_win
+            rem_win = n_train - tot_win;
+            fnames = fnames(1:idx+1);
+            i_start(1:idx+1, i_set) = ...
+                [win_ind(1:idx,i_set); {win_ind{idx+1,i_set}(1:rem_win)}];
+        end
+    end
+end
+
+if n_test > 0
+    % Make sure that there is no overlapping between train and test set
+    rep_ni_test = repmat(i_start{i_epoch,2}', 1, ni_train);
+    dist = abs(double(rep_ni_test)-double(i_start{i_epoch,1}));
+    assert(all(dist(:) >= winlen), 'Train and test windows are overlapping!');
 end
 end
