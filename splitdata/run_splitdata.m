d6f317927743179fa4bb855468f8c8e6883d947a
Carlos H. Mendoza-Cardenas <cmendoza@udel.edu>
2021-03-12 12:15:28 -0500

    Refactor to follow changes in splitdata()

--- splitdata/run_splitdata.m
+++ splitdata/run_splitdata.m
@@ -1,42 +1,37 @@
 function run_splitdata(confpath)
 
-%% Set up parpool (used by cleanLineNoise)
-myCluster = parcluster('local');
-myCluster.NumWorkers = str2double(getenv('SLURM_NTASKS_PER_NODE'));
-myCluster.JobStorageLocation = getenv('TMPDIR');
-mypool = parpool(myCluster, myCluster.NumWorkers);
-
 RunConf = jsondecode(fileread(confpath));
 
-data_dir = '/lustre/scratch/cmendoza/sikmeans/LKini2019';
+data_dir = RunConf.data_dir;
+winlen = RunConf.winlen; % window length
+prctage = RunConf.prctage; % Percentage of data
+train_prctage = RunConf.train_prctage; % Percentage of train data
+overlap = RunConf.overlap; % True: Overlapping windows, False: non-overlapping
+start_gap = RunConf.start_gap; % Gap for random start.
+patient_label = RunConf.patient_label;
+outfile = RunConf.outfile;  % Output file
 
-%mfname_frmt = 'monte%d_data_split_for_CSP_testing.mat';
+if overlap
+    overlap_str = 'overlap';
+else
+    overlap_str = 'non-overlap';
+end
 
 conditions = {'preictal', 'interictal'};
 n_cond = length(conditions);
-
-winlen = RunConf.winlen; %512 * 2; % 2 seconds
-n_train = RunConf.n_train; %100e3;
-n_test = RunConf.n_test; % 0;
-overlap = RunConf.overlap; % true;
-n_subdiv = RunConf.n_subdiv; % 1;
-goal = RunConf.goal;
-n_monte = RunConf.n_monte;
-
-patient_label = RunConf.patient_label;
-parfor i_monte = 1:n_monte
-    rng(i_monte);
-    for i_cond = 1:n_cond % each condition has its own folder
-        dirpath = fullfile(data_dir, patient_label, conditions{i_cond});
-        [dfnames, i_start] = splitdata(dirpath, winlen, n_train(i_cond), ...
-            n_test(i_cond),...
-            overlap, n_subdiv);
-        mfname = sprintf('monte%d_data_split_for_%s', i_monte, goal);
-        fpath = fullfile(dirpath, mfname);
-        mObj = matfile(fpath, 'Writable', true);
-        mObj.fnames = dfnames;
-        mObj.i_start = i_start;
-    end
+for i_cond = 1:n_cond % each condition has its own folder
+    dirpath = fullfile(data_dir, patient_label, conditions{i_cond});
+    
+    [train_names, train_indices, test_names, test_indices] = ...
+        splitdata(dirpath, winlen,...
+                  'prctage', prctage,...
+                  'train_prctage', train_prctage,...
+                  'overlap', overlap,...
+                  'start_gap', start_gap);
+              
+    outifile = fullfile(dirpath, outfile);
+    save(outifile, 'train_names', 'train_indices', 'test_names', 'test_indices', '-v7.3');
+    
 end
-delete(mypool);
+
 end
